81e419540111ea09b1cbd43f854fb72ec7c81319
pjsinco <psinco@gmail.com>
2016-06-17 11:26:14 -0500

    Use image-blowup technique to load splash image
    Described here:
    https://css-tricks.com/the-blur-up-technique-for-loading-background-images/

--- assets/bundle.js
+++ assets/bundle.js
@@ -1,7 +1,121 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+window.onload = function loadStuff() {
+  var win, doc, img, header, enhancedClass, styles;
+  // Quit early if older browser (e.g. IE8).
+  if (!('addEventListener' in window)) {
+    return;
+  }
+
+  win = window;
+  doc = win.document;
+  img = new Image();
+  header = doc.querySelector('.splash');
+  enhancedClass = 'splash-enhanced';
+
+  // Rather convoluted, but parses out the first mention of a background
+  // image url for the enhanced header, even if the style is not applied.
+  var bigSrc = (function() {
+    // Find all of the CssRule objects inside the inline stylesheet 
+    if (document.styleSheets && document.styleSheets[0].cssRules) {
+      styles = doc.styleSheets[0].cssRules;
+    } else if (document.styleSheets && document.styleSheets[0].rules) {
+      styles = doc.styleSheets[0].rules;
+    }
+
+    if (!styles) {
+      return;
+    }
+
+    //styles = doc.querySelector('style').sheet.cssRules || doc.styleSheets[0]
+    // Fetch the background-image declaration...
+    var bgDecl = (function() {
+      // ...via a self-executing function, where a loop is run
+      var bgStyle, i, l = styles.length;
+      for (i = 0; i < l; i++) {
+        // ...checking if the rule is the one targeting the
+        // enhanced header.
+        if (styles[i].selectorText &&
+          styles[i].selectorText == '.' + enhancedClass) {
+          // If so, set bgDecl to the entire background-image
+          // value of that rule
+          bgStyle = styles[i].style.backgroundImage;
+          // ...and break the loop.
+          break;
+        }
+      }
+      // ...and return that text.
+      return bgStyle;
+    }());
+    // Finally, return a match for the URL inside the background-image
+    // by using a fancy regex i Googled up, if the bgDecl variable is
+    // assigned at all.         
+    return bgDecl && bgDecl.match(/(?:\(['|"]?)(.*?)(?:['|"]?\))/)[1];
+  }());
+
+  // Assign an onLoad handler to the dummy image *before* assigning the src
+  img.onload = function() {
+    header.className += ' ' + enhancedClass;
+  };
+  // Finally, trigger the whole preloading chain by giving the dummy
+  // image its source.
+  if (bigSrc) {
+    img.src = bigSrc;
+  }
+};
+
+
+
+
+//window.onload = function loadsStuff() {
+//
+//
+//  var img, header, enhancedClass;
+//
+//  // Quit early in an older browser (IE8, for ex.)
+//  if (!('eventListener' in window)) {
+//    return;
+//  }
+//  
+//  img = new Image();
+//  header = document.querySelector('.splash');
+//  enhancedClass = '.splash-enhanced';
+//
+//  var bigSrc = (function() {
+//
+//    var styles = document.querySelector('style').sheet.cssRules;
+//    var bgDecl = (function() {
+//
+//      var bgStyle, i, l = styles.length;
+//      for (var i = 0; i < l; i++) {
+//        if (styles[i].selectorText && 
+//              styles[i].selectorText == '.' + enhancedClass) {
+//          bgStyle = styles[i].style.backgroundImage;
+//          break;
+//        }
+//      }
+//
+//  console.dir(styles);
+//      return bgStyle;
+//    })();
+//
+//    return bgDecl && bgDecl.match(/(?:\(['|"]?)(.*?)(?:['|"]?\))/)[1];
+//
+//  })();
+//
+//  img.onload = function() {
+//    header.className += ' ' + enhancedClass;
+//  };
+//
+//  if (bigSrc) {
+//    img.src = bigSrc;
+//  }
+//
+//};
+
+
 $(document).ready(function() {
 
-  console.log('hiya');
+  //console.log('hiya');
 
   $('body').addClass('js');
 
